19a4b47429de59e3644478ea7c7bf8064b67461e
Bruce McIntosh <bruce.e.mcintosh@gmail.com>
2021-02-28 07:47:15 +1000

    Update typos and idex

--- typesetting/format_idx.pl
+++ typesetting/format_idx.pl
@@ -137,6 +137,108 @@ while (<INFILE>) {
     if ($line =~ /Elizabeth Pretoria McIntosh/) {
       $line =~ s/Elizabeth Pretoria McIntosh/Elizabeth Pretoria McIntosh\\index{McIntosh, Elizabeth Pretoria}/;
     }
+    if ($line =~ /George Waldock Jr/) {
+      $line =~ s/George Waldock Jr/George Waldock Jr\\index{Waldock, George Jnr}/;
+    }
+    if ($line =~ /S Waldock/) {
+      $line =~ s/S Waldock/S Waldock\\index{Waldock, Shirley}/;
+    }
+    if ($line =~ /Shirley Waldock/) {
+      $line =~ s/Shirley Waldock/Shirley Waldock\\index{Waldock, Shirley}/;
+    }
+    if ($line =~ /John Waldock/) {
+      $line =~ s/John Waldock/John Waldock\\index{Waldock, John}/;
+    }
+    if ($line =~ /Ross Waldock/) {
+      $line =~ s/Ross Waldock/Ross Waldock\\index{Waldock, Ross}/;
+    }
+    if ($line =~ /Heather Waldock/) {
+      $line =~ s/Heather Waldock/Heather Waldock\\index{Waldock, Heather}/;
+    }
+    if ($line =~ /Ted Waldock/) {
+      $line =~ s/Ted Waldock/Ted Waldock\\index{Waldock, Ted}/;
+    }
+    if ($line =~ /G Waldock/) {
+      $line =~ s/G Waldock/G Waldock\\index{Waldock, G}/;
+    }
+    if ($line =~ /C W Waldock/) {
+      $line =~ s/C W Waldock/C W Waldock\\index{Waldock, C W}/;
+    }
+    if ($line =~ /Mr C Waldock/) {
+      $line =~ s/Mr C Waldock/Mr C Waldock\\index{Waldock, C W}/;
+    }
+    if ($line =~ /George Waldock Jnr/) {
+      $line =~ s/George Waldock Jnr/George Waldock Jnr\\index{Waldock, George Jnr}/;
+    }
+    if ($line =~ /Nick Waldock/) {
+      $line =~ s/Nick Waldock/Nick Waldock\\index{Waldock, Nick}/;
+    }
+    if ($line =~ /May Gedge/) {
+      $line =~ s/May Gedge/May Gedge\\index{Gedge, May}/;
+    }
+    elsif ($line =~ /Gedges/) {
+      $line =~ s/Gedges/Gedges\\index{Gedge, Bill}\\index{Gedge, May}/;
+    }
+    elsif ($line =~ /May and Bill Gedge/) {
+      $line =~ s/May and Bill Gedge/May and Bill Gedge\\index{Gedge, Bill}\\index{Gedge, May}/;
+    }
+    elsif ($line =~ /Mr and Mrs Gedge/) {
+      $line =~ s/Mr and Mrs Gedge/Mr and Mrs Gedge\\index{Gedge, Bill}\\index{Gedge, May}/;
+    }
+    elsif ($line =~ /Mr Gedge/) {
+      $line =~ s/Mr Gedge/Mr Gedge\\index{Gedge, Bill}/;
+    }
+    elsif ($line =~ /Gedge/) {
+      $line =~ s/Gedge/Gedge\\index{Gedge, Bill}/;
+    }
+    if ($line =~ /Trevor Cichero/) {
+      $line =~ s/Trevor Cichero/Trevor Cichero\\index{Cichero, Trevor}/;
+    }
+    if ($line =~ /son Cliff/) {
+      $line =~ s/son Cliff/son Cliff\\index{Krebs, Cliff}/;
+    }
+    if ($line =~ /Cliff Krebs/) {
+      $line =~ s/Cliff Krebs/Cliff Krebs\\index{Krebs, Cliff}/;
+    }
+    if ($line =~ /Jack Krebs/) {
+      $line =~ s/Jack Krebs/Jack Krebs\\index{Krebs, Jack}/;
+    }
+    if ($line =~ /John \(Jack\) Krebs/) {
+      $line =~ s/John \(Jack\) Krebs/John (Jack) Krebs\\index{Krebs, John}/;
+    }
+    if ($line =~ /Bill Roberts/) {
+      $line =~ s/Bill Roberts/Bill Roberts\\index{Roberts, Bill}/;
+    }
+    if ($line =~ /G W Roberts/) {
+      $line =~ s/G W Roberts/G W Roberts\\index{Roberts, Bill}/;
+    }
+    if ($line =~ /Kathleen O誰eill/) {
+      $line =~ s/Kathleen O誰eill/Kathleen O誰eill\\index{O誰eill, Kathleen}/;
+    }
+    if ($line =~ /Kathleen \(Katie\) O誰eill/) {
+      $line =~ s/Kathleen \(Katie\) O誰eill/Kathleen (Katie) O誰eill\\index{O誰eill, Kathleen}/;
+    }
+    if ($line =~ /Thelma O誰eill/) {
+      $line =~ s/Thelma O誰eill/Thelma O誰eill\\index{O誰eill, Thelma}/;
+    }
+    if ($line =~ /David O誰eill/) {
+      $line =~ s/David O誰eill/David O誰eill\\index{O誰eill, David}/;
+    }
+    if ($line =~ /Richard O誰eill/) {
+      $line =~ s/Richard O誰eill/Richard O誰eill\\index{O誰eill, Richard}/;
+    }
+    if ($line =~ /F Narracott/) {
+      $line =~ s/F Narracott/F Narracot\\index{Narracott, Frank}/;
+    }
+    if ($line =~ /Frank Narracott/) {
+      $line =~ s/Frank Narracott/Frank Narracot\\index{Narracott, Frank}/;
+    }
+    if ($line =~ /Frank and Myra Narracott/) {
+      $line =~ s/Frank and Myra Narracott/Frank and Myra Narracott\\index{Narracott, Frank}/;
+    }
+    if ($line =~ /Bernard Monz/) {
+      $line =~ s/Bernard Monz/Bernard Monz\\index{Monz, Bernard}/;
+    }
     $output .= $line;
   }
  
