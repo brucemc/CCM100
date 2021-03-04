ae6b6fd73db4eefc5538875bf65b67548b075a99
Bruce McIntosh <bruce.e.mcintosh@gmail.com>
2021-03-04 17:59:43 +1000

    Updates for captions and balancing

--- typesetting/format.pl
+++ typesetting/format.pl
@@ -3,11 +3,12 @@ use strict;
 my $output = "";
 my @chap_lines = ();
 my $caption = "";
+my $source = "";
 my $file_name = "";
 my $pic_width = "";
 my $pic_pos = "";
 my $chapter = 0;
-my $footnote_count = 0;
+#my $footnote_count = 0;
 
 open(INFILE, "< mainmatter.tex") or die "Cannot open file\n";
 
@@ -26,6 +27,11 @@ while (<INFILE>) {
     my $skip = "\\noindent\\rule[0.5ex]{\\linewidth}{1pt}\n";
     push @chap_lines, $skip;
   }
+  elsif ($line =~ /Newcol$/) {
+    my $skip = "\\vfill\\null\n";
+    my $skip .= "\\newpage\n";
+    push @chap_lines, $skip;
+  }
   elsif ($line =~ /Newpage$/) {
     my $skip = "\\newpage\n";
     push @chap_lines, $skip;
@@ -41,6 +47,15 @@ while (<INFILE>) {
   elsif ($line =~ /Medskip$/) {
     my $skip = "\\medskip\n";
     push @chap_lines, $skip;
+  }
+  elsif ($line =~ /Endnotes$/) {
+    my $en = "\\printendnotes[custom]\n";
+    $en .= "\\setcounter{endnote}{0}\n";
+    push @chap_lines, $en;
+  }
+  elsif ($line =~ /Balance$/) {
+    my $en = "\\balance\n";
+    push @chap_lines, $en;
   }
   elsif ($line =~ /Dictum\\\{(.*)\\}\\\{(.*)\\}$/) {
     my $s = $1;
@@ -64,15 +79,21 @@ while (<INFILE>) {
   elsif ($line =~ /position\\\{(.*)\\}/) {
     $pic_pos   = $1;
   }
-  elsif ($line =~ /caption\\\{(.*)\\}/) {
+  elsif ($line =~ /caption\\\{(.*)\\}\\\{(.*)\\}/) {
     $caption   = $1;
+    $source   = $2;
     my $fig = "";
     if ($pic_pos eq "p") {
       $fig .= "\\begin{figure*}[!htb]\n";
       $fig .= "\\begin{center}\n";
       $fig .= "\\includegraphics[width=$pic_width\\textwidth,center]{../images/$file_name}\n";
       if ($caption ne "") {
-        $fig .= "\\caption{\\itshape $caption}\n";
+        if ($source ne "") {
+          $fig .= "\\caption{{\\itshape $caption} {\\scriptsize($source)}}\n";
+        }
+        else {
+          $fig .= "\\caption{\\itshape $caption}\n";
+        }
       }
       $fig .= "\\end{center}\n";
       $fig .= "\\end{figure*}\n";
@@ -82,7 +103,12 @@ while (<INFILE>) {
       $fig .= "\\begin{center}\n";
       $fig .= "\\includegraphics[width=$pic_width\\linewidth,center]{../images/$file_name}\n";
       if ($caption ne "") {
-        $fig .= "\\caption{\\itshape $caption}\n";
+        if ($source ne "") {
+          $fig .= "\\caption{{\\itshape $caption} {\\scriptsize($source)}}\n";
+        }
+        else {
+          $fig .= "\\caption{\\itshape $caption}\n";
+        }
       }
       $fig .= "\\end{center}\n";
       $fig .= "\\end{figure}\n";
@@ -93,7 +119,12 @@ while (<INFILE>) {
       $fig .= "\\begin{center}\n";
       $fig .= "\\includegraphics[width=$pic_width\\linewidth,center]{../images/$file_name}\n";
       if ($caption ne "") {
-        $fig .= "\\captionof{figure}{\\itshape $caption}\n";
+        if ($source ne "") {
+          $fig .= "\\captionof{figure}{{\\itshape $caption} {\\scriptsize($source)}}\n";
+        }
+        else {
+          $fig .= "\\captionof{figure}{\\itshape $caption}\n";
+        }
       }
       $fig .= "\\end{center}\n";
       $fig .= "\\end{minipage}\n";
@@ -107,32 +138,32 @@ while (<INFILE>) {
   }
   elsif ($line =~ /\\footnote/) {
     push @chap_lines, $line;
-    $footnote_count += 1;
+    #$footnote_count += 1;
   }
   elsif ($line =~ /\\chapter/) {
     my $lines = @chap_lines;
-    my $count = 0;
-    my $balance = 0;
+    #my $count = 0;
+    #my $balance = 0;
     my $l = "";
     foreach $l (@chap_lines) {
       $output .= $l;
-      $count = $count + 1;
-      if ($count > $lines - 3) {
-        if ($chapter > 0 && $balance eq 0) {
-         $output .= "\\balance\n";
-         $balance = 1;
-        }
-      }
+      #$count = $count + 1;
+      #if ($count > $lines - 3) {
+      #  if ($chapter > 0 && $balance eq 0) {
+      #   $output .= "\\balance\n";
+      #   $balance = 1;
+      #  }
+      #}
     }
     @chap_lines = ();
-    if ($chapter > 0) {
+    #if ($chapter > 0) {
       #$output .= "\\balance\n";
-      if ($footnote_count > 0) {
-        $output .= "\\printendnotes[custom]\n";
-        $output .= "\\setcounter{endnote}{0}\n";
-        $footnote_count = 0;
-      }
-    }
+      #if ($footnote_count > 0) {
+      #  $output .= "\\printendnotes[custom]\n";
+      #  $output .= "\\setcounter{endnote}{0}\n";
+      #  $footnote_count = 0;
+      #}
+    #}
     $chapter += 1;
     if ($line =~ /Appendicies/) {
         $output .= "\\backmatter\n";
@@ -145,25 +176,25 @@ while (<INFILE>) {
   }
 }
 my $lines = @chap_lines;
-my $count = 0;
-my $balance = 0;
+#my $count = 0;
+#my $balance = 0;
 my $l = "";
 foreach $l (@chap_lines) {
   $output .= $l;
-  $count = $count + 1;
-  if ($count > $lines - 2) {
-    if ($chapter > 0 && $balance eq 0) {
-      $output .= "\\balance\n";
-      $balance = 1;
-    }
-  }
+  #$count = $count + 1;
+  #if ($count > $lines - 2) {
+  #  if ($chapter > 0 && $balance eq 0) {
+  #    $output .= "\\balance\n";
+  #    $balance = 1;
+  #  }
+  #}
 }
 @chap_lines = ();
 
 close INFILE;
 
-if ($footnote_count > 0) {
-  $output .= "\\printendnotes[custom]\n";
-}
+#if ($footnote_count > 0) {
+#  $output .= "\\printendnotes[custom]\n";
+#}
 
 print $output;
